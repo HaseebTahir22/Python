818ae79a1a6d8bb1e2c98c461bf3ca30a02578c4
HaseebTahir22 <126325445+HaseebTahir22@users.noreply.github.com>
2024-08-10 13:22:41 +0500

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,30 @@
+print("Hello World")
+cars = ["BMW", "Toyota", "Volvo"]
+print(cars)
+# a = 10
+# b = 20
+# print(a+b)
+   #Taking user input
+# x = input("Enter first number: ")
+# y = input("Enter second number: ")
+# print(int(x) + int(y))
+   # Length of string
+# name = "Haseeb"
+# Len = len(name)
+# print(Len)
+# print(name[0:5])
+# print(name[0:6])
+ # remove special characters
+# str1 = "Haseeb!!!"
+# print(str1.rstrip("!"))
+  # replace name
+# print(str1.replace("Haseeb", "John"))
+ # split name
+# print(str1.split(" "))
+       # If else statement
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+if(a>=18):
+  print("You can drive")
+else:
+  print("You cannot drive")
